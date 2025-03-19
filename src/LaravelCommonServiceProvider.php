3488bf9648f41ad42f85649195eec6dbd6ef6c6c
udhuong <ungdinhhuong@gmail.com>
2025-03-19 00:00:54 +0000

    refactor: cập nhật provider

--- src/LaravelCommonServiceProvider.php
+++ src/LaravelCommonServiceProvider.php
@@ -1,9 +1,29 @@
 <?php
+namespace Udhuong\LaravelCommon;
 
-
+use Illuminate\Contracts\Debug\ExceptionHandler;
 use Illuminate\Support\ServiceProvider;
+use Udhuong\LaravelCommon\Domain\Exceptions\ApiExceptionHandler;
 
 class LaravelCommonServiceProvider extends ServiceProvider
 {
+    public function boot(): void
+    {
+        $this->registerExceptionHandler();
+    }
 
+    protected function registerExceptionHandler(): void
+    {
+        $this->app->booted(function () {
+            $handler = $this->app->make(ExceptionHandler::class);
+
+            if (method_exists($handler, 'withExceptions')) {
+                $handler->withExceptions(function ($exceptions) {
+                    $exceptions->renderable(function (\Throwable $exception, $request) {
+                        return ApiExceptionHandler::handle($exception);
+                    });
+                });
+            }
+        });
+    }
 }
